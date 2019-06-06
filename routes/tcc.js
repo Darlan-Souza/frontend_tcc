f13f50c7de13ab6f7fee10816f9f4f8e3e6a6543
Darlan <darlan.sgf@gmail.com>
2019-06-06 10:59:51 -0300

    rodapé e cadastro de tcc

--- routes/tcc.js
+++ routes/tcc.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
+router.get('/exibir_todos', function(req, res){
+    res.render("tcc/exibir_todos")
+})
+
 router.get('/cadastro', function (req, res) {
     res.render("tcc/cadastro")
 })
